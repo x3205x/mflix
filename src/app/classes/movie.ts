dbd9ea5e945e2cf427bc9c7f1617b06457df2151
Torrance Gates <x3205x@gmail.com>
2022-08-11 21:35:00 -0400

    basic service setup

---
+++ src/app/classes/movie.ts
@@ -0,0 +1,22 @@
+export class Movie {
+    "tomatoes": Object;
+    "genres": Array<String>;
+    "cast": Array<String>;
+    "languages": Array<String>;
+    "directors": Array<String>;
+    "countries": Array<String>;
+    "_id": String;
+    "plot": String;
+    "runtime": Number;
+    "num_mflix_comments": Number;
+    "poster": String;
+    "title": String;
+    "lastupdated": String;
+    "released": String;
+    "writers": Array<String>;
+    "awards": Array<Object>;
+    "year": Number;
+    "imdb": Object;
+    "type": String;
+    "lasupdated": String;
+}
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Movie } from '../classes/movie';
+import { movie_mocks } from './movie_mocks';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  movies = of(movie_mocks)
+
+  constructor() { }
+
+  getMovieById(id: String){
+
+    let movie = {}
+
+    movie_mocks.map(data => {
+      if(data._id === id){
+        movie = data
+      }
+    })
+    return movie
+  }
+
+  getMovies() {
+    return this.movies.subscribe()
+  }
+}
