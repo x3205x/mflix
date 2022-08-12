ad504895fe673da636dc918998830be2fbb5acb2
Torrance Gates <x3205x@gmail.com>
2022-08-12 12:05:33 -0400

    intergrated details component

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,5 +1,7 @@
+import { getLocaleId } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 
 @Component({
@@ -9,12 +11,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
 
+  movie: any = {}
+
   constructor(
+    public api: MoviesService,
     private route: ActivatedRoute
     ) { }
 
   ngOnInit(): void {
     let id = this.route.snapshot.paramMap.get("id")
+    this.getId(id)
+  }
+
+  getId(id: any){
+    this.movie = this.api.getMovieById(id)
+    console.log(this.movie)
   }
 
 }
